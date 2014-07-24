1c8cd12e8487ebeb5bb888c7dfb7aed6ca7ac15c
talik <vitaliy.semeshko@gmail.com>
2014-07-24 14:14:04 +0300

    another test commit

--- com/test/Main.java
+++ com/test/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Initial");
 		System.out.println("2014-07-20: VS: Test after Epicentet");
+		System.out.println("2014-07-24: VS: Another Test");
 	}
 
 }
